b1280cc5c6a8a330544c1981c2007524922eab66
Doug Williams <similate@gmail.com>
2021-01-14 19:10:40 -0700

    Checking in changes from assignment 2; added a publisher to book.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,21 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher javaLearning = new Publisher("Java Learning", "7 E North Fork Rd", "Centennial", "WY", "82055-0048");
+        publisherRepository.save(javaLearning);
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "123123", "Java Learning");
+        Book ddd = new Book("Domain Driven Design", "123123", javaLearning);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -29,16 +36,21 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "3939459459", "Java Learning");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459", javaLearning);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        javaLearning.getBooks().add(ddd);
+        javaLearning.getBooks().add(noEJB);
+
+        publisherRepository.save(javaLearning);
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Authors: " + authorRepository.count());
         System.out.println("Number of Books: " + bookRepository.count());
-
+        System.out.println("Number of Publishers: " + publisherRepository.count());
     }
 }
